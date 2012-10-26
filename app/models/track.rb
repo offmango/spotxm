a21620c4a8acc614c27ae66833934355e85d9424
offmango <offmango@gmail.com>
2012-10-25 21:47:10 -0400

    added saving to database

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,32 +1,54 @@
 class Track
 
+	include MongoMapper::Document
+
+  	key :track_name, String
+  	key :artist_name, String
+  	key :album_name, String
+  	key :channel_name, String
+  	key :channel_number,  Integer
+  	key :played_at, Time
+
 	def self.now_playing(time = Time.now)
 		timestamp_data = XMWrapper.get_timestamp
 		convert_timestamp_to_tracks(timestamp_data)
 	end
 
+	def self.save_current_playlist
+		tracks = Track.now_playing
+		tracks.each {|track| Track.create(track) if new_track?(track)}
+	end
+
 	private
 
 	def self.convert_timestamp_to_tracks(timestamp_array)
-		track_array = []
+		tracks = []
 		timestamp_array.each do |channel|
 			channel_name = channel.at_css("channelname").text
 			channel_number = channel.at_css("channelnumber").text
-			track = channel.css("currentevent")
-			artist_name = track.at_css("artists name").text
-			album_name = track.at_css("album name").text
-			track_name = track.css("name").last.text
+			track_info = channel.css("currentevent")
 			track =	{
-						track_name: track_name, 
-						artist_name: artist_name, 
-						album_name: album_name, 
+						track_name: track_info.css("name").last.text, 
+						artist_name: track_info.at_css("artists name").text, 
+						album_name: track_info.at_css("album name").text, 
 						channel_name: channel_name, 
 						channel_number: channel_number.to_i, 
 						played_at: Time.now
 					}
-			track_array << track
+			tracks << track
 		end
-		track_array.sort_by{|track| track[:channel_number]}
+		tracks.sort_by{|track| track.channel_number}
+	end
+
+	def self.new_track?(track_hash)
+		last_track = Track.find_all_by_channel_number(track_hash[:channel_number]).last
+		if last_track.blank?
+			return true
+		else
+			last_track.track_name != track_hash[:track_name] or
+			last_track.artist_name != track_hash[:artist_name] or
+			last_track.album_name != track_hash[:album_name]
+		end
 	end
 
 end
--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -3,23 +3,32 @@ require 'time'
 
 describe Track do
 
-	Time.stubs(:now).returns(Time.parse("2012-10-22 12:45:54"))
+	now = Time.parse("2012-10-22 12:45:54")
 	timestamp_data = Nokogiri::HTML("<html><body></body></html>")
 	track_array = 	[
 						{
 							track_name: "Atlantic City",
 							artist_name: "Bruce Springsteen",
-							channel_number: 20
+							album_name: "Darkness on the Edge of Town",
+							played_at: now,
+							channel_number: 20,
+							channel_name: "E Street Radio"
 						},
 						{
 							track_name: "Don't Stop Believin'",
 							artist_name: "Journey",
-							channel_number: 8
+							album_name: "Greatest Hits",
+							played_at: now,
+							channel_number: 8,
+							channel_name: "80's on 8"
 						},
 						{
 							track_name: "My Dog Done Got Shot",
-							artist_name: "Country Bumpkin",
-							channel_number: 33
+							artist_name: "Garth Brooks",
+							channel_number: 33,
+							album_name: "Songs About My Dog",
+							played_at: now,
+							channel_name: "Country Bumpkin"
 						}
 					]
 
@@ -36,4 +45,58 @@ describe Track do
 			tracks.should == track_array
 		end
 	end
+
+	context "#save_current_playlist" do
+		it "saves the array of tracks now playing to the database" do
+			Track.all.each {|track| track.delete }
+			Track.stubs(:now_playing).returns(track_array)
+
+			Track.save_current_playlist
+
+			tracks = Track.all
+			tracks.size.should == 3
+			tracks.should include(Track.find_by_track_name("Atlantic City"))
+			tracks.should include(Track.find_by_track_name("Don't Stop Believin'"))
+			tracks.should include(Track.find_by_track_name("My Dog Done Got Shot"))
+		end
+
+		it "does not save the now playing track for a channel if that is the most recent saved song for that channel" do
+			Track.all.each {|track| track.delete }
+			Track.create({
+							track_name: "Nebraska",
+							artist_name: "Bruce Springsteen",
+							album_name: "Darkness on the Edge of Town",
+							channel_number: 20, 
+							channel_name: "E Street Radio",
+							played_at: now - 300
+						})
+			Track.create({
+							track_name: "Magic",
+							artist_name: "The Cars",
+							album_name: "The Cars Greatest Hits",
+							channel_number: 8, 
+							channel_name: "80's on 8",
+							played_at: now - 200
+						})			
+			Track.create({
+							track_name: "Atlantic City",
+							artist_name: "Bruce Springsteen",
+							album_name: "Darkness on the Edge of Town",
+							channel_number: 20, 
+							channel_name: "E Street Radio",
+							played_at: now
+						})
+			Track.stubs(:now_playing).returns(track_array)
+
+			Track.save_current_playlist
+
+			tracks = Track.all
+			tracks.size.should == 5
+			atlantic_city = Track.find_by_track_name("Atlantic City")
+			tracks.should include(Track.find_by_track_name("Don't Stop Believin'"))
+			tracks.should include(Track.find_by_track_name("My Dog Done Got Shot"))
+
+		end
+	end
+
 end
