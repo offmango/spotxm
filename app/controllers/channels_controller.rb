f8d9af09abdcdf4525960a939d34141cd5ed7af1
offmango <offmango@gmail.com>
2012-12-07 21:45:44 -0500

    more styling with twitter bootstrap

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -4,7 +4,11 @@ class ChannelsController < ApplicationController
     	@tracks = Track.most_recent.sort { |x, y| x.channel.channel_number <=> y.channel.channel_number }
     	if params[:channel].present? and params[:channel][:channel_number].present?
     		@channel = Channel.find_by_channel_number(params[:channel][:channel_number])
-    		redirect_to channel_path(@channel) if @channel.present?		
+    		if @channel.present?	
+    			redirect_to channel_path(@channel) 
+    		else
+    			flash[:alert] = "Channel #{params[:channel][:channel_number]} not found"
+    		end
     	end
 	end
 
