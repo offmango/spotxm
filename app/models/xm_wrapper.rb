c31909c2a174c964a586cbc9e88a1d6e416c48ec
offmango <offmango@gmail.com>
2012-12-08 11:28:44 -0500

    more nokogiri fix attempts

--- app/models/xm_wrapper.rb
+++ app/models/xm_wrapper.rb
@@ -4,12 +4,15 @@ class XMWrapper
 
 	BASE_URL = "https://www.siriusxm.com"
 	TIMESTAMP_URL = "/metadata/pdt/en-us/xml/events/timestamp/"
+	HEADER_HASH = {
+		"User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1"
+	}
 
 	
 	def self.get_timestamp(time = Time.now)
 		puts "Getting Sirius XM timestamp..."
 		url = get_timestamp_url(time)
-		timestamp_data = Nokogiri::HTML(open(url, 'User-Agent' => "Spotxm/0.1"))
+		timestamp_data = Nokogiri::HTML(open(url, HEADER_HASH))
 		timestamp_data.css('metadata')
 	end
 
